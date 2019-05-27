c5eb68ca34358dbb1574122195a7f9b70ae7f171
Kaply9l <kislov.igor@list.ru>
2019-05-27 21:41:20 +0700

    Fix main and people_choise

--- src/people_choise.cpp
+++ src/people_choise.cpp
@@ -8,14 +8,14 @@ int people_choise(int Matches, int move, bool Correct) //Ход игрока
     cin >> Move;
     Correct = true;
 
-    if (Move >= 1 && Move <= 10 &&
-        Move <= Matches) //Проверка правильности введенного числа
+    if (Move >= 1 && Move <= 10 && Move <= Matches) //Проверка правильности введенного числа
     {
       return move;
-    } else {
-      cout << "Неверно, вы можете взять от одной до десяти спичек и не больше "
+    }else 
+	{
+      		cout << "Неверно, вы можете взять от одной до десяти спичек и не больше "
               "чем осталось на столе. Повторите ввод. \n";
-      Correct = false;
-    }
+      		Correct = false;
+    	}
   } while (!Correct);
 }
