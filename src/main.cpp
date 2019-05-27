7eca6ece561611ec6ce7591bbb5f939d0ac6f651
Kaply9l <kislov.igor@list.ru>
2019-05-27 21:25:52 +0700

    Fixed again

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "head.h"
 
 int main() {
- const int MaxMatches = 100;
- int Player, Matches, Move;
- bool Correct;
+  const int MaxMatches = 100;
+  int Player, Matches, Move;
+  bool Correct;
 
   srand(time(0));
   Matches = MaxMatches;
@@ -15,14 +15,14 @@ int main() {
       move = people_choise(Matches, Move, Correct);
       cout << " \n";
       Player = 2;
-    }else {
+    } else {
       Move = comp_choise(Move, Matches);
       cout << " \n";
       Player = 1;
     }
     Matches -= Move;
 
-  }while (Matches != 0);
+  } while (Matches != 0);
   if (Player == 2) //Алгоритм, определяющий кто победил
   {
     cout << "Поздравляю, вы победили!\n";
