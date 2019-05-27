623f95f7b04900026c25bb7d0215ee13a9b630d2
Kaply9l <kislov.igor@list.ru>
2019-05-27 23:05:55 +0700

    Edit people_choise

--- src/people_choise.cpp
+++ src/people_choise.cpp
@@ -1,22 +1,11 @@
 #include "head.h"
-
 int people_choise(int Matches, int Move) //Ход игрока
 {
-  bool Correct;
-  do {
-    cout << "Ваш ход на столе " << Matches << " спичек. \n";
-    cout << "Сколько спичек вы берете? \n";
-    cin >> Move;
-    Correct = true;
+      cout << "Ваш ход на столе " << Matches << " спичек. \n";
+      cout << "Сколько спичек вы берете? \n";
+      cin >> Move; //Проверка правильности введенного числа
+      if (Move < 1 || Move > 10)
+        cout << "Неправильное кол-во спичек" << endl;
 
-    if (Move >= 1 && Move <= 10 && Move <= Matches) //Проверка правильности введенного числа
-    {
-      return Move;
-    }else
-	{
-      	cout << "Неверно, вы можете взять от одной до десяти спичек и не больше "
-              "чем осталось на столе. Повторите ввод. \n";
-      	Correct = false;
-    	}
-  } while (!Correct);
+    return Move;
 }
