f32f2a9455759ff58cf8fd559e69b062c3b2b72c
Kaply9l <kislov.igor@list.ru>
2019-05-27 23:18:19 +0700

    Fixed people_choise.cpp

--- src/people_choise.cpp
+++ src/people_choise.cpp
@@ -4,8 +4,9 @@ int people_choise(int Matches, int Move) //Ход игрока
       cout << "Ваш ход на столе " << Matches << " спичек. \n";
       cout << "Сколько спичек вы берете? \n";
       cin >> Move; //Проверка правильности введенного числа
-      if (Move < 1 || Move > 10)
-        cout << "Неправильное кол-во спичек" << endl;
-
+      while (Move < 1 || Move > 10){
+        cout << "Неправильное кол-во спичек, введите заново" << endl;
+	cin >> Move;
+      }
     return Move;
 }
